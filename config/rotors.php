8e0abbf6109011f764e9f73a4b0137a4e167b1e2
Sam Driver <sdriver@gmx.co.uk>
2023-03-04 16:13:20 +0000

    Add rotor configurations

--- config/rotors.php
+++ config/rotors.php
@@ -1,15 +1,40 @@
 <?php
 
 return [
-    'I' => [
+    // Mappings are given for ring setting 0, with the first letter what an 'A' would map to, then 'B', etc.,
+    [
+        'name'    => 'Rotor I',
         'mapping' => [
             'E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'
         ],
-        // TODO: notch position
+        'notch'   => 'Q',
     ],
-    'II' => [
+    [
+        'name'    => 'Rotor II',
         'mapping' => [
             'A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'
         ],
-    ]
+        'notch'   => 'E',
+    ],
+    [
+        'name'    => 'Rotor III',
+        'mapping' => [
+            'B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'
+        ],
+        'notch'   => 'V',
+    ],
+    [
+        'name'    => 'Rotor IV',
+        'mapping' => [
+            'E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W', 'B'
+        ],
+        'notch'   => 'J',
+    ],
+    [
+        'name'    => 'Rotor V',
+        'mapping' => [
+            'V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K'
+        ],
+        'notch'   => 'Z',
+    ],
 ];
